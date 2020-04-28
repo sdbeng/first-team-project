a293b499a4d8226c8ff91e08350d2664d3585371
Sergio Barrera <sdbeng@gmail.com>
2020-04-28 14:33:44 -0700

    Change paragraph in app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Great project to master github on teams.
         </p>
         <a
           className="App-link"
